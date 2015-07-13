b663138d462dc00276209d28ff463d5c8b9e2789
Nolan Phillips <ncphillips.19@gmail.com>
2015-07-13 13:29:43 -0300

    merge conflict resolution

--- routes.js
+++ routes.js
@@ -1,9 +1,6 @@
 /* jslint node: true */
 'use strict';
-<<<<<<< HEAD
-=======
 
->>>>>>> 28f5e58873dcc217a9eced77040bdea2f0c755ff
 var log = global.log;
 var worker = require('crowdstudy_worker').controllers;
 var controllers = require('./controllers');
@@ -26,13 +23,6 @@ module.exports = function (app) {
   app.param('id', function (req, res, next, id) {
     var ObjectId = require('mongodb').ObjectID;
     var workers = req.db.collection('workers');
-<<<<<<< HEAD
-    workers.find({_id: ObjectId(id)}).toArray(function (err, workers) {
-      if (err) {
-        return next(err);
-      }
-      else if (workers.length < 1) {
-=======
 
     workers.find({_id: new ObjectId(id)}).toArray(function (err, workers) {
       if (err) {
@@ -40,7 +30,6 @@ module.exports = function (app) {
       }
 
       if (workers.length < 1) {
->>>>>>> 28f5e58873dcc217a9eced77040bdea2f0c755ff
         return next(new Error('Failed to load Worker.'));
       }
 
